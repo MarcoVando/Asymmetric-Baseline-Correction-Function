5d26c9a3482f587c432caad8fdad91b11538804d
Marco Vandone <servizi.marco@gmail.com>
2025-05-02 17:12:12 +0200

    Create bkg.py

---
+++ bkg.py
@@ -0,0 +1,10 @@
+def __get_background(y, lam, p, niter=15):
+    L = len(y)
+    D = sparse.diags([1,-2,1],[0,-1,-2], shape=(L,L-2))
+    w = np.ones(L)
+    for i in range(niter):
+        W = sparse.spdiags(w, 0, L, L)
+        Z = W + lam * D.dot(D.transpose())
+        z = spsolve(Z, w*y)
+        w = p * (y > z) + (1-p) * (y < z)
+    return z
