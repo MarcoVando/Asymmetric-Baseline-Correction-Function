c18994dfcbc3e4d7e6e3e1befa38710f65de60a6
Marco Vandone <servizi.marco@gmail.com>
2026-04-03 10:41:56 +0200

    Add imports for numpy and scipy in bkg.py

--- bkg.py
+++ bkg.py
@@ -1,3 +1,7 @@
+import numpy as np
+from scipy import sparse
+from scipy.sparse.linalg import spsolve
+
 def __get_background(y, lam, p, niter=15):
     L = len(y)
     D = sparse.diags([1,-2,1],[0,-1,-2], shape=(L,L-2))
